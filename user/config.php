c0274b1c0900176e21d5a6759086773372f67437
Nick Bair <njbair@gmail.com>
2022-09-12 09:26:04 -0400

    upd8s

--- user/config.php
+++ user/config.php
@@ -5,20 +5,20 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-define( 'YOURLS_DB_USER', getenv('YOURLS_DB_USER') );
-define( 'YOURLS_DB_PASS', getenv('YOURLS_DB_PASS') );
-define( 'YOURLS_DB_NAME', getenv('YOURLS_DB_NAME') );
-define( 'YOURLS_DB_HOST', getenv('YOURLS_DB_HOST') );
-define( 'YOURLS_DB_PREFIX', getenv('YOURLS_DB_PREFIX') );
-define( 'YOURLS_SITE', getenv('YOURLS_SITE') );
-define( 'YOURLS_HOURS_OFFSET', getenv('YOURLS_HOURS_OFFSET') ); 
-define( 'YOURLS_COOKIEKEY', getenv('YOURLS_COOKIEKEY') );
+define( 'YOURLS_DB_USER', $_ENV['YOURLS_DB_USER'] );
+define( 'YOURLS_DB_PASS', $_ENV['YOURLS_DB_PASS'] );
+define( 'YOURLS_DB_NAME', $_ENV['YOURLS_DB_NAME'] );
+define( 'YOURLS_DB_HOST', $_ENV['YOURLS_DB_HOST'] );
+define( 'YOURLS_DB_PREFIX', $_ENV['YOURLS_DB_PREFIX'] );
+define( 'YOURLS_SITE', $_ENV['YOURLS_SITE'] );
+define( 'YOURLS_HOURS_OFFSET', $_ENV['YOURLS_HOURS_OFFSET'] );
+define( 'YOURLS_COOKIEKEY', $_ENV['YOURLS_COOKIEKEY'] );
 
 /** YOURLS language or "locale".
  ** Change this setting to "localize" YOURLS (use a translation instead of the default English). A corresponding .mo file
  ** must be installed in the user/language directory.
  ** See http://yourls.org/translations for more information */
-define( 'YOURLS_LANG', '' ); 
+define( 'YOURLS_LANG', '' );
 
 /** Allow multiple short URLs for a same long URL
  ** Set to true to have only one pair of shortURL/longURL (default YOURLS behavior)
@@ -32,7 +32,7 @@ define( 'YOURLS_PRIVATE', true );
 
 /** Username(s) and password(s) allowed to access the site. Passwords either in plain text or as salted hashes.
  ** Read http://yourls.org/userpassword for more information */
-$yourls_user_passwords = json_decode(getenv('YOURLS_USER_PASSWORDS'), true);
+$yourls_user_passwords = json_decode($_ENV['YOURLS_USER_PASSWORDS'], true);
 
 /*
  ** URL Shortening settings
@@ -46,7 +46,7 @@ define( 'YOURLS_URL_CONVERT', 62 );
  * Stick to one setting. It's best not to change after you've started creating links.
  */
 
-/** 
+/**
 * Reserved keywords (so that generated URLs won't match them)
 * Define here negative, unwanted or potentially misleading keywords.
 */
